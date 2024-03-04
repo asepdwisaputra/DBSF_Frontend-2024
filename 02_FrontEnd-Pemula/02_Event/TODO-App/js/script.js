cc39265beb6d75bb55e0a532729cba0979255068
Asep Dwi Saputra <akunasep111@gmail.com>
2024-03-04 10:34:06 +0700

    data sudah ditampilkan di 'Yang harus dilakukan'

--- 02_FrontEnd-Pemula/02_Event/TODO-App/js/script.js
+++ 02_FrontEnd-Pemula/02_Event/TODO-App/js/script.js
@@ -36,5 +36,33 @@ const todos = [];
 const RENDER_EVENT = "render-todo";
 
 document.addEventListener(RENDER_EVENT, () => {
-  console.log(todos);
+  //   console.log(todos);
+
+  const uncompletedTODOList = document.getElementById("todos");
+  uncompletedTODOList.innerHTML = "";
+
+  for (const todoItem of todos) {
+    const todoElement = makeTodo(todoItem);
+    uncompletedTODOList.append(todoElement);
+  }
 });
+
+// "Yang harus dilakukan" view
+function makeTodo(todoObject) {
+  const textTitle = document.createElement("h2");
+  textTitle.innerText = todoObject.task;
+
+  const textTimestamp = document.createElement("p");
+  textTimestamp.innerText = todoObject.timestamp;
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("inner");
+  textContainer.append(textTitle, textTimestamp);
+
+  const container = document.createElement("div");
+  container.classList.add("item", "shadow");
+  container.append(textContainer);
+  container.setAttribute("id", `todo-${todoObject.id}`);
+
+  return container;
+}
